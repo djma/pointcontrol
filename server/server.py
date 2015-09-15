400bf188c5ddf8b4e1b988900f324d948a69dad9
Ubuntu <ubuntu@ip-172-31-27-14.us-west-2.compute.internal>
2015-09-15 03:44:08 +0000

    added server

---
+++ server/server.py
@@ -0,0 +1,182 @@
+import tornado.ioloop
+import tornado.web
+import sqlite3
+import json
+import os
+
+allnames = [];
+idToName = {};
+
+def _execute(query, args):
+        dbPath = '/home/ubuntu/data.db'
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+        try:
+                cursorobj.execute(query, args)
+                result = cursorobj.fetchall()
+                connection.commit()
+        except Exception:
+                raise
+        connection.close()
+        return result
+
+def loadNames():
+    global allnames
+    query = ''' select max(start_date), first_name, last_name, f.fencerid, f.birthyear
+            from fencers f, tournament_results r
+            where f.fencerid = r.fencerid
+            group by f.fencerid
+            '''
+    dbPath = '/home/ubuntu/data.db'
+    connection = sqlite3.connect(dbPath)
+    cursorobj = connection.cursor()
+    try:
+            cursorobj.execute(query)
+            allnames = cursorobj.fetchall()
+            connection.commit()
+    except Exception:
+            raise
+    connection.close()
+    for a in allnames:
+        idToName[a[3]] = a[1] + " " + a[2]
+
+def matches(query):
+        to_return = sorted(filter(lambda x: query.upper() in (x[1].upper() + " " + x[2].upper()), allnames), key = lambda y : (y[1], y[2]))
+        if to_return:
+            return to_return
+        to_return = filter(lambda x: all(q.upper() in (x[1].upper() + " " + x[2].upper()) for q in query.split(" ")), allnames)
+        return to_return
+
+class Main(tornado.web.RequestHandler):
+    def get(self):
+        self.render("main.html")
+"""
+class AddStudent(tornado.web.RequestHandler):
+    def get(self):
+        self.render('sqliteform.html')
+
+    def post(self):
+        marks = int(self.get_argument("marks"))
+        name = self.get_argument("name")
+        query = ''' insert into stud (name, marks) values ('%s', %d) ''' %(name, marks);
+        _execute(query)
+        self.render('success.html')
+"""
+
+class GetNames(tornado.web.RequestHandler):
+    def get(self):
+	q = self.get_argument("q")
+        to_return = matches(q)
+        result = []
+        for x in to_return:
+            item = {"t_date" : x[0], "fullname" : x[1] + " " + x[2], "id": x[3], "birthyear": x[4]}
+            result.append(item)
+        self.write(json.dumps(result))
+
+    def _processresponse(self,rows,fencerid, weapon):
+        data_points = {"fencerid" : fencerid, "ratings" : [], "weapon": weapon}
+        for row in rows:
+                point = {"date": row[1], "rating": row[0]}
+                data_points["ratings"].append(point) 
+        self.write(json.dumps(data_points))
+
+class GetRating(tornado.web.RequestHandler):
+    def get(self):
+	fencerid = self.get_argument("id")
+	weapon = self.get_argument("weapon")
+        query = ''' select r.ts_mu, t.start_date
+		from adjusted_ratings r, bouts b, events e, 
+		tournaments t
+		where r.boutid = b.boutid
+                and r.weapon = (?)
+		and b.eventid = e.eventid
+		and e.tournamentid = t.tournamentid
+		and r.fencerid = (?)
+		order by t.start_date asc, r.boutid desc
+		'''
+        args = (weapon, int(fencerid),)
+        rows = _execute(query, args)
+        self._processresponse(rows,int(fencerid), weapon)
+
+    def _processresponse(self,rows,fencerid, weapon):
+        data_points = {"fencerid" : fencerid, "ratings" : [], "weapon": weapon}
+        for row in rows:
+                point = {"date": row[1], "rating": row[0]}
+                data_points["ratings"].append(point) 
+        self.write(json.dumps(data_points))
+
+class GetRatingFake(tornado.web.RequestHandler):
+    def get(self):
+        data_points = {"fencerid": 3, "ratings": [{"date": "2014-03-12", "rating":30}]}
+        self.write(json.dumps(data_points))
+
+class GetPlayers(tornado.web.RequestHandler):
+    def get(self):
+        query = ''' select f.*
+		from fencers f
+		'''
+        dbPath = '/home/ubuntu/data.db'
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+        try:
+                cursorobj.execute(query)
+                rows = cursorobj.fetchall()
+                connection.commit()
+        except Exception:
+                raise
+        connection.close()
+
+        players = [];
+        for row in rows:
+            player = {"name": row[1] + " " + row[2], "id": row[0]}
+            players.append(player)
+        self.write(json.dumps(players))
+        
+class GetName(tornado.web.RequestHandler):
+    def get(self):
+        query = ''' select f.*
+		from fencers f
+		where f.firstname = b.boutid
+		'''
+        dbPath = '/home/ubuntu/data.db'
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+        try:
+                cursorobj.execute(query)
+                rows = cursorobj.fetchall()
+                connection.commit()
+        except Exception:
+                raise
+        connection.close()
+
+        players = [];
+        for row in rows:
+            player = {"name": row[1] + " " + row[2], "id": row[0]}
+            players.append(player)
+        self.write(json.dumps(players))
+
+class GetIDToName(tornado.web.RequestHandler):
+    def get(self):
+	fencerid = int(self.get_argument("id"))
+        obj = {"name": idToName[fencerid]}
+        self.write(json.dumps(obj))
+       
+class IconHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_header("Content-Type", "image/vnd.microsoft.icon")
+        with open("favicon.ico", 'rb') as f:
+            self.write(f.read())
+        return self.flush() 
+     
+application = tornado.web.Application([
+    (r"/", Main),
+    (r"/get",GetRating),
+    (r"/name",GetNames),
+    (r"/translate",GetIDToName),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+    (r"/favicon.ico", IconHandler),
+],debug=True)
+if __name__ == "__main__":
+    loadNames();
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ server/static/main.js
@@ -0,0 +1,190 @@
+
+
+currentId = 0;
+
+function getData(id) {
+  if (id == 0) {
+    return;
+  }
+  weapon = $('input[name=optionsWeapons]:checked', '#fencerform').val();
+  $(".loading-image").show();
+  $.getJSON("get?id="+id+"&weapon="+weapon,
+      function(result) {
+        $.getJSON("translate?id="+id , function (res) {
+          name = res["name"]
+          currentId = id;
+          drawBasic(result, name);
+          $(".loading-image").hide();
+        });
+      }
+  );
+}
+
+function drawBasic(fencer, fullname) {
+      // 116580
+      dateseen = {}
+      rows = [];
+      fencer_name = fullname;
+      console.log(fencer_name);
+      if (fencer != 0) {
+        for (i = 0; i < fencer['ratings'].length; i++) {
+          if (fencer['ratings'][i]['date'] in dateseen)
+            continue;
+          dateseen[fencer['ratings'][i]['date']] = 1;
+          rows.push([Date.parse(fencer['ratings'][i]['date']), fencer['ratings'][i]['rating']]);
+        }
+      }
+      latest_rank = 0;
+      if (rows.length != 0) {
+        latest_rank = rows[rows.length - 1][1] 
+      }
+    $('#container').highcharts({
+
+        credits: {
+             enabled: false
+        },
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Rating'
+        },
+        subtitle: {
+            text: fencer_name + ' - ' + latest_rank.toFixed(2) + ' - ' + fencer["weapon"],
+            style: {
+                fontSize: '16px'
+            }
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+            },
+            title: {
+                text: 'Date'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Rating'
+            },
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:%e. %b}: {point.y:.2f}'
+        },
+
+        plotOptions: {
+            series: {
+                cursor:'pointer',
+                marker: {
+                    enabled: true
+                },
+                point: {
+                    events: {
+                        click: function (e) {
+                          console.log(this);
+                        }
+                    }
+                }
+            }
+        },
+
+        series: [{
+            name: fencer_name,
+            // Define the data points. All series have a dummy year
+            // of 1970/71 in order to be compared on the same x axis. Note
+            // that in JavaScript, months start at 0 for January, 1 for February etc.
+            data: rows
+        }]
+    });
+} 
+function drawFrame() {
+    rows = []
+    $('#container').highcharts({
+
+        credits: {
+             enabled: false
+        },
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Adjusted Rating'
+        },
+        subtitle: {
+            text: "Example - 0.00"
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+            },
+            title: {
+                text: 'Date'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Rating'
+            },
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:%e. %b}: {point.y:.2f}'
+        },
+
+        plotOptions: {
+            series: {
+                cursor:'pointer',
+                marker: {
+                    enabled: true
+                },
+            }
+        },
+
+        series: [{
+            name: "Example",
+            // Define the data points. All series have a dummy year
+            // of 1970/71 in order to be compared on the same x axis. Note
+            // that in JavaScript, months start at 0 for January, 1 for February etc.
+            data: rows
+        }]
+
+    });
+}
+
+function displayNames(names) {
+  $("#title_thing").text("Click on the correct fencer...");
+  $("#subtitle_thing").text("Full Name - Birth Year - Last Tournament");
+  $( "h3" ).empty();
+  for (i = 0 ; i < names.length ; i++) {
+    $("#title_box").append('<h3 id="'+names[i]["id"]+'">' + names[i]["fullname"] + ' - ' + names[i]["birthyear"] + ' - ' + names[i]["t_date"]+ "</h3>");
+  }
+  $("h3").click(function() {
+    getData($(this).attr('id'));
+  });
+}
+
+
+$( document ).ready(function() {
+    drawFrame();
+  $(".pure-radio").click(function() {
+    getData(currentId);
+  });
+});
+$( "#fencerform" ).submit(function( event ) {
+  value_string = $('#fenceridinput').val();
+  if (!isNaN(value_string)) {
+    getData(parseInt(value_string));
+  } else {
+    value_name = $.trim(value_string);
+  $(".loading-image").show();
+    $.getJSON("name?q="+value_name,
+        function(result) {
+          console.log(result);
+          displayNames(result);
+          $(".loading-image").hide();
+        }
+    );
+  } 
+  event.preventDefault();
+});
